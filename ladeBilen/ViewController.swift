7e7e54a6706cab42d20ccffae46d6e459192ce27
Ludvig <ludellevold@outlook.com>
2017-06-09 20:55:31 +0200

    Flere store endringer

--- ladeBilen/ViewController.swift
+++ ladeBilen/ViewController.swift
@@ -12,23 +12,51 @@ import MapKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var mapWindow: MKMapView!
-
+    
+    @IBOutlet weak var favouritesButton: UIButton!
+    
+    @IBOutlet weak var searchButton: UIButton!
+    
+    @IBOutlet weak var nearestButton: UIButton!
+    
+    @IBOutlet weak var settingsButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        initializeButtons()
     }
     
-    @IBAction func favButton(_ sender: UIBarButtonItem) {
-        self.performSegue(withIdentifier: "toFavorites", sender: self)
-
+    func initializeButtons(){
+        settingsButton.layer.cornerRadius = 25
+        settingsButton.clipsToBounds = true
+        settingsButton.imageEdgeInsets = UIEdgeInsetsMake(15, 15, 15, 15)
         
+        nearestButton.layer.cornerRadius = 25
+        nearestButton.clipsToBounds = true
+        nearestButton.imageEdgeInsets = UIEdgeInsetsMake(15, 15, 15, 15)
+        
+        favouritesButton.layer.cornerRadius = 25
+        favouritesButton.clipsToBounds = true
+        favouritesButton.imageEdgeInsets = UIEdgeInsetsMake(20, 20, 20, 20)
+        
+        searchButton.layer.cornerRadius = 25
+        searchButton.clipsToBounds = true
+        searchButton.imageEdgeInsets = UIEdgeInsetsMake(24, 24, 24, 24)
     }
+    
+    override func viewWillLayoutSubviews() {
+        positionLegalMapLabel()
+    }
+    
+    func positionLegalMapLabel() {
+        //Må høre med apple om man kan flytte "Legal" linken til et separert view. Typ "About/legal" view elns. Nedenfor er metoden for å hente ut labelen + for å flytte den.
+        let legalMapLabel = self.mapWindow.subviews[1]
+        
+        
+        //legalMapLabel.frame.origin = CGPoint(x: 20, y: 20)
+        
+        legalMapLabel.frame.origin = CGPoint(x: self.mapWindow.bounds.size.width / 2, y: legalMapLabel.frame.origin.y)
 
-
+    }
 }
 
