ad98a15fbec4277ffb5befe81ae7cfed5cc86fb3
Ludvig <ludellevold@outlook.com>
2017-06-09 18:38:50 +0200

    Initial Commit

---
+++ ladeBilen/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ladeBilen
+//
+//  Created by Ludvig Ellevold on 09.06.2017.
+//  Copyright © 2017 Ludvig Ellevold. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
