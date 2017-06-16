1bd0684bc52e23c8f392590de8b765e26d86154f
Ludvig <ludellevold@outlook.com>
2017-06-16 21:23:35 +0200

    Div edits

--- ladeBilen/welcomeViewController.swift
+++ ladeBilen/welcomeViewController.swift
@@ -14,8 +14,15 @@ class welcomeViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var retypePasswordTextField: UITextField!
     
-    @IBOutlet weak var loginButtonBottomConstraint: NSLayoutConstraint!
+    @IBOutlet weak var loginOptionButton: UIButton!
+    @IBOutlet weak var registrationOptionButton: UIButton!
+    @IBOutlet weak var loginRegisterAccountButton: UIButton!
+
     
+    @IBOutlet weak var loginButtonBottomConstraint: NSLayoutConstraint!
+    @IBOutlet weak var statusBackgroundHeightConstraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var textfieldStackHeightConstraint: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,11 +45,75 @@ class welcomeViewController: UIViewController, UITextFieldDelegate {
         NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIKeyboardWillShow, object: nil)
         NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIKeyboardWillHide, object: nil)
     }
-
+    
     func setupButtons(){
         emailTextField.setBottomBorder()
         passwordTextField.setBottomBorder()
+        retypePasswordTextField.setBottomBorder()
     }
+    
+    
+    
+    
+    
+    
+    @IBAction func loginOptionButton(_ sender: UIButton) {
+        loadLoginConfig()
+    }
+    
+    
+    @IBAction func registrationOptionButton(_ sender: UIButton) {
+        loadRegistrationConfig()
+    }
+    
+    func loadLoginConfig(){
+        retypePasswordTextField.isHidden = true
+        textfieldStackHeightConstraint.constant = 110
+        
+        
+        self.view.layoutIfNeeded()
+        removeCover()
+    }
+    
+    func loadRegistrationConfig(){
+        loginRegisterAccountButton.setTitle("Next", for: .normal)
+        removeCover()
+        
+    }
+    
+    func removeCover(){
+        loginOptionButton.isHidden = true
+        registrationOptionButton.isHidden = true
+        statusBackgroundHeightConstraint.constant = 40
+        UIView.animate(withDuration: 0.5) {
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     func keyboardWillShow(notification: NSNotification){
         //Flytter login button slik at den ligger ovenfor keyboard når keyboard blir aktivert
@@ -61,5 +132,8 @@ class welcomeViewController: UIViewController, UITextFieldDelegate {
     }
     
     
+    @IBAction func loginRegisterAccountButton(_ sender: UIButton) {
+        
+    }
     
 }
