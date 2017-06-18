e86d1f70dd23dd7981c37b90357f5dc810bbe48e
Ludvig <ludellevold@outlook.com>
2017-06-18 21:14:06 +0200

    Jobber med settings

---
+++ ladeBilen/detailedSettingsTableViewController.swift
@@ -0,0 +1,28 @@
+//
+//  detailedSettingsTableViewController.swift
+//  ladeBilen
+//
+//  Created by Ludvig Ellevold on 18.06.2017.
+//  Copyright © 2017 Ludvig Ellevold. All rights reserved.
+//
+
+import UIKit
+
+class detailedSettingsTableViewController: UITableViewController {
+    
+    var selectedRow: Int = 0
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    
+
+    
+
+    
+
+    
+
+}
--- ladeBilen/settingsViewController.swift
+++ ladeBilen/settingsViewController.swift
@@ -9,50 +9,63 @@
 import UIKit
 import FirebaseAuth
 
-class settingsViewController: UIViewController {
+class settingsViewController: UITableViewController {
+    
+    let sections = ["Søke instillinger", "Bruker", "Utsene", "Om oss"]
+    let options = [["Type stasjoner", "Type støpsel", "Eier", "Status"], ["Endre E-Post", "Endre Passord"], ["Flytte knappene"], ["Brukeravtale", "Sammarbeidspartnere"]]
+    let settingsOptions = ["Søke instillinger", "Bruker", "Utsene", "Om oss", "Logg ut"]
     
     let defaults = UserDefaults.standard
     var window: UIWindow?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.tableFooterView = UIView()
 
     }
     
-    
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
-
-    func flipButtonStack(){
-        if (defaults.integer(forKey: "flip") == 0 || defaults.integer(forKey: "flip") == 1){
-            //Flytter buttonsStack til venstre
-            defaults.set(2, forKey: "flip")
-        } else {
-            //Flytter buttonsStack til høyre
-            defaults.set(1, forKey: "flip")
-        }
-        
-    }
-    @IBAction func flipButtonsStackButton(_ sender: UIButton) {
-        flipButtonStack()
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "settingsCell", for: indexPath)
+        cell.textLabel?.text = self.settingsOptions[indexPath.row]
+        return cell
     }
     
-    @IBAction func logoutButtonClicked(_ sender: UIButton) {
-        do {
-            try FIRAuth.auth()?.signOut()
-            let storyboard = UIStoryboard(name: "Main", bundle: nil)
-            // vc is the Storyboard ID that you added
-            // as! ... Add your ViewController class name that you want to navigate to
-            let controller = storyboard.instantiateViewController(withIdentifier: "Welcome") as! welcomeViewController
-            self.present(controller, animated: false, completion: { () -> Void in
-            })
-        } catch let signOutError as NSError {
-            print ("Error signing out: %@", signOutError)
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.settingsOptions.count
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if (indexPath.row == 0) {
+            //Naviger til "Søke instillinger"
+            print("0")
+        } else if (indexPath.row == 1) {
+            //Naviger til "Bruker"
+            print("1")
+        } else if (indexPath.row == 2) {
+            //Naviger til "Utsene"
+            print("2")
+        } else if (indexPath.row == 3) {
+            //Naviger til "Om oss"
+            print("3")
+        } else if (indexPath.row == 4) {
+            //Logg ut
+            print("4")
         }
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toDetailedSettings" {
+            if let indexPath = self.tableView.indexPathForSelectedRow {
+                let destination = segue.destination as! detailedSettingsTableViewController
+                destination.selectedRow = indexPath.row
+            }
+        }
+    }
+    
     
     
     
